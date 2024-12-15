e9f4c4be80971340c689179fda5f97285aadba5c
Numwali <n.umwali@alustudent.com>
2024-12-15 19:31:28 +0200

    calculating age

---
+++ assignment2.js
@@ -0,0 +1,17 @@
+//declaring the age
+const myAge = 2;
+//declaration of early years
+let earlyYears = 2;
+earlyYears *= 10.5;
+// calculating the first two years accounted
+ let laterYears = myAge - 2;
+ //calculating the number of dog's years accounated for by later years
+ laterYears *= 4;
+ console.log (earlyYears)
+ console.log(laterYears)
+ console.log(myAge)
+ //calculating the age
+ let myAgeInDogYears = earlyYears + laterYears;
+ 
+ myName = 'Noella' .toLowerCase();
+ console.log(`My name is ${myName}. I am ${myAgeInDogYears} years old in dog years.`);
